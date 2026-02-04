215e72672ee7fc93e7cd012742ac8b3488fff8dd
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-04 15:39:59 -0300

    Tenemos una app para generar el archivo sqlite necesario para consumir en la app primaria

--- Generador-Archivo-Sqlite.py
+++ Generador-Archivo-Sqlite.py
@@ -2,34 +2,49 @@ import sqlite3
 import os
 
 
-
 def leerArchivos(archivo):
-    with open(archivo,"r",encoding="utf-8") as ArchivosSql:
-        consulta=ArchivosSql.read()
+    """
+    Lee una ruta a un archivo
+
+    :param archivo: Ruta a un archivo(sql)
+    Returns:
+    Devuelve el contenido de un archivo
+    """
+    with open(archivo, "r", encoding="utf-8") as ArchivosSql:
+        consulta = ArchivosSql.read()
         return consulta
 
 
+def crearDB(sentencia):
+    """
+    Crea una db en sqlite con el nombre datos.db
 
-def crearDB(tabla):
+    :param sentencia: Lee una sentencia en sqlite para crear tablas
+    """
+
     db_name = "datos.db"
     try:
         conn = sqlite3.connect(db_name)
         cursor = conn.cursor()
-        cursor.execute(tabla)
+        cursor.execute(sentencia)
         conn.commit()
         conn.close()
     except Exception as e:
         print(f"Error: {e}")
 
 
-# try:
-#     crearDB()
-#     print("Deberia de estar creada la db con una tabla")
-# except Exception as e:
-#     print(f"Error {e}")
+archivos = [
+    "sql-sentencias/1.sql",
+    "sql-sentencias/2.sql",
+    "sql-sentencias/3.sql",
+    "sql-sentencias/4.sql",
+]
 
-archivos=["sql-sentencias/1.sql","sql-sentencias/2.sql","sql-sentencias/3.sql","sql-sentencias/4.sql"]
-for archivo in archivos:
-    print(archivo)
+#   Leo los path de cada archivos .sql, estos tienen sentencias sql para crear tablas
+try:
+    for archivo in archivos:
+        #   leerArchivo lee lo que hay en los sql y se los pasa a crearDB
+        crearDB(leerArchivos(archivo))
 
-
+except Exception as e:
+    print(f"Error al querer generar las tablas de la db{e}")
