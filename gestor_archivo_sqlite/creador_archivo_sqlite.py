0d519f912d0e3f598ca02719c2d06ed98730883c
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-18 15:46:42 -0300

    Hay que hacer que ejecuta las consultas en cada archivo dentro de la db

--- gestor_archivo_sqlite/creador_archivo_sqlite.py
+++ gestor_archivo_sqlite/creador_archivo_sqlite.py
@@ -1,10 +1,18 @@
 import os
 
-dir_script = os.path.dirname(__file__)  #   Carpeta actual
-ruta_archivo = os.path.join(
-    dir_script, "..", "gestor_archivo_sqlite", "archivo_db.db"
-)
+carpeta_sql = "gestor_archivo_sqlite/sqls"
 
-with open(ruta_archivo,"w+") as f:
-    datos = f.read()
-    print(datos)
+dir_script = os.path.dirname(__file__)  #   Carpeta actual
+ruta_archivo = os.path.join(dir_script, "..", "gestor_archivo_sqlite", "archivo_db.db")
+
+
+if os.path.exists(carpeta_sql):
+    archivos = os.listdir(carpeta_sql)
+    for archivo in archivos:
+        if archivo.endswith(".sql"):
+            ruta_completa = os.path.join(carpeta_sql, archivo)
+            print(f"Leyendo {archivo}")
+            with open(ruta_completa, "r", encoding="utf-8") as f:
+                contenido = f.read()
+else:
+    print("La carpeta sqls no existe")
