8e0a09cb25f9dd84157c6a3809db1f4bfe7c668d
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-25 15:38:39 -0300

    Me trabe en una boludes, lo dejamos para luego

--- gui/ventana_sec_alt_cliente.py
+++ gui/ventana_sec_alt_cliente.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+import sys, os
+
+#   Esto me deja importar modelos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from modelos.aux_abm_cliente import verificar_campo_vacio
 
 
 class Ventana_sec_alt_cliente(tk.Toplevel):
@@ -14,11 +20,11 @@ class Ventana_sec_alt_cliente(tk.Toplevel):
         self.etiquetaNombrelabelFrame.grid(
             column=0, row=0, padx=10, pady=10, sticky="w"
         )
-        self.stringVarNombre = tk.StringVar()
-        self.entradaNombre = tk.Entry(
-            self.labelFrame, textvariable=self.stringVarNombre
+        self.stringVarNombrelabelFrame = tk.StringVar()
+        self.entradaNombrelabelFrame = tk.Entry(
+            self.labelFrame, textvariable=self.stringVarNombrelabelFrame
         )
-        self.entradaNombre.grid(column=1, row=0, padx=10, pady=10, sticky="e")
+        self.entradaNombrelabelFrame.grid(column=1, row=0, padx=10, pady=10, sticky="e")
 
         #   Campo Apellido
         self.etiquetaApellidolabelFrame = tk.Label(self.labelFrame, text="Apellido:   ")
@@ -57,12 +63,27 @@ class Ventana_sec_alt_cliente(tk.Toplevel):
         )
         self.entradaCorreolabelFrame.grid(column=1, row=3, padx=10, pady=10, sticky="e")
 
-        self.botonIngresarlabelFrame = tk.Button(self.labelFrame, text="Ingresar")
+        self.botonIngresarlabelFrame = tk.Button(
+            self.labelFrame, text="Ingresar", command=self.validar_datos_alta_cliente
+        )
         self.botonIngresarlabelFrame.grid(column=0, row=4, padx=10, pady=10, sticky="e")
+        # self.botonIngresarlabelFrame.config(state="disabled")
         self.botonCancelarlabelFrame = tk.Button(
             self.labelFrame, text="Cancelar", command=self.cancelar_alta_usuario
         )
         self.botonCancelarlabelFrame.grid(column=1, row=4, padx=10, pady=10, sticky="e")
+
+    def validar_datos_alta_cliente(self):
+        try:
+            campos = [
+                self.stringVarNombrelabelFrame.get(),
+                self.stringVarApellidolabelFrame.get(),
+                self.stringVarCorreolabelFrame.get(),
+                self.stringVarTelefonolabelFrame.get(),
+            ]
+            print(campos)
+        except Exception as e:
+            print("Error al ingresar cliente")
 
     def cancelar_alta_usuario(self):
         self.destroy()
