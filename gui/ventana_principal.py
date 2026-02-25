318cca234fce52539be4d1d006873b34bc1b0458
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-25 14:18:51 -0300

    Ahi quedo la primer parte del abm para los clientes, ahora hay que avanzar con cada ventana en particular

--- gui/ventana_principal.py
+++ gui/ventana_principal.py
@@ -8,20 +8,44 @@ class Ventana_Principal(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Bienvenido a OPMFABM")
-        self.geometry("300x350")
-        self.labelFramePrincipal = tk.LabelFrame(self, text="ABM-Clientes:")
-        self.labelFramePrincipal.grid(column=0, row=0, padx=10, pady=10)
-        self.botonVentanaABMUsuario = tk.Button(
-            self.labelFramePrincipal,
-            text="Alta-Clientes   ",
+        # self.geometry("300x350")
+        self.labelFramePrincipalABMCliente = tk.LabelFrame(self, text="ABM-Cliente:")
+        self.labelFramePrincipalABMCliente.grid(column=0, row=0, padx=10, pady=10)
+        #   Boton Alta-Cliente
+        self.botonVentanaABMUsuarioAltaUsuario = tk.Button(
+            self.labelFramePrincipalABMCliente,
+            text="Alta-Cliente   ",
             command=self.abrir_ventana_sec_alt_cliente,
         )
-        self.botonVentanaABMUsuario.grid(column=0, row=0, padx=10, pady=10)
+        self.botonVentanaABMUsuarioAltaUsuario.grid(column=0, row=0, padx=10, pady=10)
+        #   Boton Edicion-Cliente
+        self.botonVentanaABMUsuarioEdicionUsuario = tk.Button(
+            self.labelFramePrincipalABMCliente, text="Edicion-Cliente"
+        )
+        self.botonVentanaABMUsuarioEdicionUsuario.grid(
+            column=1, row=0, padx=10, pady=10
+        )
+
+        #   Boton Baja-Cliente
+        self.botonVentanaABMUsuarioBajaUsuario = tk.Button(
+            self.labelFramePrincipalABMCliente, text="Baja-Cliente"
+        )
+        self.botonVentanaABMUsuarioBajaUsuario.grid(column=1, row=1, padx=10, pady=10)
+
+        #   Boton Listado-Cliente
+        self.botonVentanaABMUsuarioListadoUsuario = tk.Button(
+            self.labelFramePrincipalABMCliente, text="Listado-Cliente"
+        )
+        self.botonVentanaABMUsuarioListadoUsuario.grid(
+            column=0, row=1, padx=10, pady=10
+        )
+
+        #   Boton Salir
         self.botonCerrarVentanaPrincipal = tk.Button(
             self, text="Salir", command=self.salirAplicacionPrincipal
         )
         self.botonCerrarVentanaPrincipal.grid(
-            column=0, row=1, padx=10, pady=10, sticky="S"
+            column=0, row=1, padx=10, pady=10, sticky="se"
         )
 
     def abrir_ventana_sec_alt_cliente(self):
